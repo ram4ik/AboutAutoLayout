48d403720f7cd34ab29520d802516846acbe942f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-04 14:09:59 +0300

    init

--- AboutAutoLayout/ViewController.swift
+++ AboutAutoLayout/ViewController.swift
@@ -11,7 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let redView = UIView()
+        
+        // 2
+        view.addSubview(redView)
+        
+        redView.backgroundColor = .red
+        
+        // 1
+        //redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        
+        // 2
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        // 1
+        //view.addSubview(redView)
     }
 
 
