d028f3d1a2a20e2fa166b9feb90e6e6b4a0ad576
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-04 15:25:28 +0300

    centre anchor

--- AboutAutoLayout/ViewController.swift
+++ AboutAutoLayout/ViewController.swift
@@ -24,13 +24,25 @@ class ViewController: UIViewController {
         
         // 2
         redView.translatesAutoresizingMaskIntoConstraints = false
-        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50).isActive = true
-        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 50).isActive = true
+        
+        // 4
+        //redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        //redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+        
+        // 3
+        //redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 50).isActive = true
+        //redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 50).isActive = true
         
         // 1
         //view.addSubview(redView)
+        
+        // 3
+        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        // 4
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 
